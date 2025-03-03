928755662d7ecb033640a6920796350942abe460
TiepBM <buimanhtiep@gmail.com>
2025-03-04 04:50:29 +0700

    - rename

--- template/android/app/src/main/java/com/ce7/example/MainActivity.kt
+++ template/android/app/src/main/java/com/ce7/example/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.pvire
+package com.ce7.example
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "PVIRe"
+  override fun getMainComponentName(): String = "CE7Example"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
--- template/android/app/src/main/java/com/ce7/example/MainApplication.kt
+++ template/android/app/src/main/java/com/ce7/example/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.pvire
+package com.ce7.example
 
 import android.app.Application
 import com.facebook.react.PackageList
